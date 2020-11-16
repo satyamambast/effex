64c4d7701cfbc401e1b39d4abd59d07e77857dc3
satyamambast <satyamambast@github.com>
2020-11-17 00:21:09 +0530

    failsafe

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
 
+
+from sys import stdout
 from flask import Flask, render_template,request
+from flask import Response
+import logging
+from flask_socketio import SocketIO
 import os
+from conversions import *
+import cv2
+import numpy as np
 app = Flask(__name__)
+app.logger.addHandler(logging.StreamHandler(stdout))
+socketio = SocketIO(app)
+
+@socketio.on('input image', namespace='/test')
+def test_message(input):
+    input_str = input.split(",")[1]
+    p_image=base64_to_pil_image(input_str)
+    c_image=toRGB(p_image)
+    print(len(c_image))
+    cv2.imwrite("hello.jpeg",c_image)
+    
+    
+
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -9,11 +30,22 @@ def home():
         req = request.form
         print(req)
     return render_template("index.html")
-@app.route("/mode", methods=["GET", "POST"])
-def mode():
-    if request.method == "POST":
-        req = request.form
-        print(req)
+
+def gen():
+    """Video streaming generator function."""
+
+    app.logger.info("starting to generate frames!")
+    while True:
+        frame = camera.get_frame() #pil_image_to_base64(camera.get_frame())
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
     
 
 if __name__ == "__main__":
