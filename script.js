8b5a0c46e0f98541d680713d746d54dc3fd6869a
satyamambast <satyamambast@github.com>
2020-11-19 20:57:36 +0530

    some progress

---
+++ script.js
@@ -0,0 +1,91 @@
+function onCvLoaded () {
+    console.log('cv', cv);
+    cv.onRuntimeInitialized = onReady;
+}
+function finalchange(src,dst){
+    out = new cv.Mat(height, width, cv.CV_8UC1);
+    // var mode = document.getElementById("Effect").value;
+    if(document.getElementById("mode1").checked==true){
+        
+        cv.cvtColor(src, out, cv.COLOR_RGBA2GRAY);
+    }
+    else if(document.getElementById("mode2").checked==true)
+    {
+        out=src;
+        
+    }
+    else if(document.getElementById("thresh").checked==true)
+    {
+        cv.threshold(src, out, 177, 200, cv.THRESH_BINARY)
+    }
+    else
+    {
+        out=src;
+    }
+
+    return out;
+
+}
+const video = document.getElementById('video');
+const actionBtn = document.getElementById('actionBtn');
+const width = 1280;
+const height = 720;
+const FPS = 30;
+let stream;
+let streaming = false;
+function onReady () {
+    let src;
+    let dst;
+    const cap = new cv.VideoCapture(video);
+
+    actionBtn.addEventListener('click', () => {
+        if (streaming) {
+            stop();
+            actionBtn.textContent = 'Start';
+        } else {
+            start();
+            actionBtn.textContent = 'Stop';
+        }
+    });
+
+    function start () {
+        navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(_stream => {
+            stream = _stream;
+            console.log('stream', stream);
+            video.srcObject = stream;
+            video.play();
+            streaming = true;
+            src = new cv.Mat(height, width, cv.CV_8UC4);
+            dst = new cv.Mat(height, width, cv.CV_8UC1);
+            setTimeout(processVideo, 0)
+        })
+        .catch(err => console.log(`An error occurred: ${err}`));
+    }
+
+    function stop () {
+        if (video) {
+            video.pause();
+            video.srcObject = null;
+        }
+        if (stream) {
+            stream.getVideoTracks()[0].stop();
+        }
+        streaming = false;
+    }
+
+    function processVideo () {
+        if (!streaming) {
+            src.delete();
+            dst.delete();
+            return;
+        }
+        const begin = Date.now();
+        cap.read(src)
+        dst=finalchange(src)
+        // cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
+        cv.imshow('canvasOutput', dst);
+        const delay = 1000/FPS - (Date.now() - begin);
+        setTimeout(processVideo, delay);
+    }
+}
